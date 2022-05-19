ae560caffe4f288802353ca6bec7fdcfdd3559e2
Nguyễn Công Dũng <dung.dev.gramer@gmail.com>
2022-05-19 22:47:57 +0700

    Update Loader.java

--- src/Loader.java
+++ src/Loader.java
@@ -7,14 +7,14 @@ import java.awt.*;
 import java.io.File;
 
 public class Loader {
-    public static Image getImage(String name) {                                                                         //Load ảnh
+    public static Image getImage(String name) {                                                                         // Load image
         Image img = new ImageIcon(
                 new Loader().getClass()
                         .getResource("/Data/" + name)
         ).getImage();
         return img;
     }
-    public static void playAudio(String name) {                                                                         //Load âm thanh
+    public static void playAudio(String name) {                                                                         // Load sound
         try {
             File f = new File("src/Data/" + name);
             AudioInputStream stream = AudioSystem.getAudioInputStream(f);
